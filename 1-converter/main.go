da4bd73d1b60c8c7e00d7f688a5a08849bf4b882
t3_sla <ctf0000000@gmail.com>
2024-08-06 22:27:07 +0500

    Second homework: DONE and modded

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 const USDEUR float64 = 0.92  //06.08.2024
 const USDRUB float64 = 85.55 //06.08.2024
@@ -8,8 +11,21 @@ const USDRUB float64 = 85.55 //06.08.2024
 func main() {
 	EURRUB := USDRUB / USDEUR
 
-	eurAmount := 100.0
+	eurAmount := getUserInput("Enter EUR amount: ")
 	rubAmount := eurAmount * EURRUB
 
-	fmt.Printf("EUR: %.2f = RUB: %.2f", eurAmount, rubAmount)
+	fmt.Println(fmt.Sprintf("EUR: %.2f = RUB: %.2f", eurAmount, rubAmount))
+}
+
+func getUserInput(msg string) float64 {
+	var input string
+	fmt.Print(msg)
+	fmt.Scan(&input)
+	fmt.Println()
+	res, _ := strconv.ParseFloat(input, 64)
+	return res
+}
+
+func currencyCalc(amount float64, originalCurrency, targetCurrency string) {
+
 }
